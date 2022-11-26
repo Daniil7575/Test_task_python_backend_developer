5ea47d8f51f43d76cca7637194cc728208626e06
Daniil7575 <spider2013vip@mail.ru>
2022-11-26 18:35:58 +0300

    Some settings fixes

--- stripe_project/config/settings.py
+++ stripe_project/config/settings.py
@@ -16,7 +16,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -25,7 +24,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 ALLOWED_HOSTS = ['*']
 
@@ -41,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'order.apps.OrderConfig',
+    'coupon.apps.CouponConfig',
 ]
 
 MIDDLEWARE = [
--- stripe_project/data_example.py
+++ stripe_project/data_example.py
@@ -2,23 +2,35 @@ import psycopg2
 import os
 
 
+# Данный файл необходим для заполнения БД данными для демострации работы
+
+
 def get_db_connection():
     conn = psycopg2.connect(host=os.environ.get('POSTGRES_HOST'),
                             database=os.environ.get('POSTGRES_DB_NAME'),
                             user=os.environ.get('POSTGRES_USER'),
                             password=os.environ.get('POSTGRES_PASSWORD'),
-							port=os.environ.get('POSTGRES_PORT'))
+                            port=os.environ.get('POSTGRES_PORT'))
     return conn
 
 
 conn = get_db_connection()
 cur = conn.cursor()
-data = (
+data_item = (
     (1, 'Cup', 'A good glass cup', 300.67),
     (2, 'Bowl', 'A good white glass bowl', 330.43),
     (3, 'Water bottle', '500 ml of clean artezian water', 150),
 )
-[cur.execute(f"insert into public.payment_item values('{id}', '{name}', '{desc}', '{price}') on conflict(id) do nothing;") for id, name, desc, price in data]
+data_coupon = (
+    # Валидный купон
+    (1, '5poff', '2022-11-26 15:00:00', '2023-11-26 15:00:00', 5, True),
+    # Неактивный купон
+    (2, '100percentoff', '2022-11-26 15:00:00', '2023-11-26 15:00:00', 100, False),
+    # Просроченный купон
+    (3, '10off', '2022-11-26 15:00:00', '2022-11-26 19:00:00', 5, True),
+)
+[cur.execute(f"insert into public.payment_item values('{id}', '{name}', '{desc}', '{price}') on conflict(id) do nothing;") for id, name, desc, price in data_item]
+[cur.execute(f"insert into public.coupon_coupon values('{id}', '{code}, '{vfrom}', '{vto}', '{discount}', '{active}') on conflict(id) do nothing;") for id, code, vfrom, vto, discount, active in data_coupon]
 conn.commit()
 cur.close()
 conn.close()
